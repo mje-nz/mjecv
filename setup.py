33d9cff58d9ea11b69fc898754b5c5dd73ed3f7a
Matthew Edwards <mje-nz@users.noreply.github.com>
2020-08-24 14:32:52 +1200

    Refactor: fix extras in setup.py, add 'full' extra

--- setup.py
+++ setup.py
@@ -8,6 +8,16 @@ with open("Readme.md", "r") as f:
     long_description = f.read()
 description = long_description.splitlines()[1].strip("> ")
 
+extras_require = {
+    "opencv": ["opencv-python>=4.3.0,<5", "pickle5>=0.0.11,<0.1"],
+    "ray": ["ray>=0.8.6,<0.9"],
+    "transforms3d": ["transforms3d>=0.3.1,<0.4"],
+}
+full = []
+for deps in extras_require.values():
+    full += deps
+extras_require["full"] = full
+
 setuptools.setup(
     name="mjecv",
     version=about["__version__"],
@@ -25,8 +35,5 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
     install_requires=["numpy>=1.18,<2", "packaging>=20.4"],
-    extras_requires={
-        "opencv": ["opencv-python>=4.3.0,<5", "pickle5>=0.0.11,<0.0.1"],
-        "ray": ["ray>=0.8.6,<0.9"],
-    },
+    extras_require=extras_require
 )
