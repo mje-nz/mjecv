89cdde1180cddcd9b74d4c1f3b82b35b815a2d68
Matthew Edwards <mje-nz@users.noreply.github.com>
2020-08-27 16:12:16 +1200

    Feature: support prior in solve_pnp, add sanity checks

--- mjecv/geometry/solvepnp.py
+++ mjecv/geometry/solvepnp.py
@@ -1,11 +1,20 @@
+from typing import Tuple
+
 import cv2
 import numpy as np
+
+from ..util import as_float, require
 
 __all__ = ["solve_pnp"]
 
 
-def solve_pnp(
-    object_points: np.ndarray, image_points: np.ndarray, camera_matrix: np.ndarray
+def _solve_pnp_generic(
+    object_points: np.ndarray,
+    image_points: np.ndarray,
+    intrinsic_matrix: np.ndarray,
+    distortion_coeffs: np.ndarray = None,
+    prior: np.ndarray = None,
+    method=None,
 ):
     """Find an object pose from 3D-2D point correspondences.
 
@@ -20,29 +29,86 @@ def solve_pnp(
     Arguments:
         object_points: vector of 3D points in object coordinates
         image_points: vector of corresponding 2D points in image coordinates
-        camera_matrix: camera matrix
+        intrinsic_matrix: 3x3 camera intrinsic matrix
     Returns: rotation vector, (x, y, z) translation in metres
     """
     # Check arguments
-    assert object_points is not None
+    require(object_points is not None, "Object points are required")
+    object_points = as_float(object_points)
+    require(np.all(np.isfinite(object_points)), "Object points must be finite")
     if object_points.shape[1] != 3:
-        assert object_points.shape[0] == 3, "Object points must be 3xN or Nx3"
+        require(object_points.shape[0] == 3, "Object points must be 3xN or Nx3")
         object_points = object_points.transpose()
-    assert image_points is not None
-    if image_points.shape[1] != 2:
-        assert image_points.shape[0] == 2, "Image points must be 2xN or Nx2"
-        image_points = image_points.transpose()
-    if np.any(np.isnan(image_points)):
-        raise ValueError("Image points may not be NaN")
-    if np.any(np.isinf(image_points)):
-        raise ValueError("Image points may not be infinite")
 
-    found, rotation_vector, translation_vector = cv2.solvePnP(
-        object_points,  # Nx3 matrix of points in object coords
-        image_points,  # Nx2 matrix of points in image coords
-        camera_matrix,
-        None,
+    require(image_points is not None, "Image points are required")
+    image_points = as_float(image_points)
+    require(np.all(np.isfinite(image_points)), "Image points must be finite")
+    if image_points.shape[1] != 2:
+        require(image_points.shape[0] == 2, "Image points must be 2xN or Nx2")
+        image_points = image_points.transpose()
+
+    require(intrinsic_matrix is not None, "Intrinsic matrix is required")
+    intrinsic_matrix = as_float(intrinsic_matrix)
+    if distortion_coeffs is not None:
+        distortion_coeffs = as_float(distortion_coeffs)
+
+    prior_rvec = None
+    prior_tvec = None
+    if prior:
+        require(cv2.SOLVEPNP_ITERATIVE, "Prior only used with SOLVEPNP_ITERATIVE")
+        prior_rvec, prior_tvec = prior
+
+    solutions, rvecs, tvecs, reprojection_errors = cv2.solvePnPGeneric(
+        object_points,
+        image_points,
+        intrinsic_matrix,
+        distortion_coeffs,
+        flags=method,
+        useExtrinsicGuess=prior is not None,
+        rvec=prior_rvec,
+        tvec=prior_tvec,
     )
-    if not found:
-        raise RuntimeError("Marker not found?")
-    return rotation_vector, translation_vector
+    if solutions == 0:
+        raise RuntimeError("No solutions")
+
+    rvecs = [np.squeeze(r) for r in rvecs]
+    tvecs = [np.squeeze(t) for t in tvecs]
+    return rvecs, tvecs, reprojection_errors.reshape(-1)
+
+
+def solve_pnp(
+    object_points: np.ndarray,
+    image_points: np.ndarray,
+    camera_matrix: np.ndarray,
+    distortion_coeffs: np.ndarray = None,
+    prior: np.ndarray = None,
+    tol: float = None,
+    shape: Tuple[int, int] = None,
+):
+    """TODO
+
+    Note that at least 6 points are required, or 4 if they're planar, or 3 with a prior.
+
+    Args:
+        tol: maximum acceptable RMS reprojection error in pixels.
+        shape: (rows, cols) of image size, used for sanity checks.
+    """
+    require(np.all(image_points > 0), "Image points must be within image bounds")
+    if shape:
+        require(image_points < shape)
+
+    rvecs, tvecs, reprojection_errors = _solve_pnp_generic(
+        object_points,
+        image_points,
+        camera_matrix,
+        distortion_coeffs,
+        prior,
+        cv2.SOLVEPNP_ITERATIVE,
+    )
+
+    assert len(rvecs) == 1
+    if tol and reprojection_errors[0] > tol:
+        raise RuntimeError(
+            f"Reprojection error too high: {reprojection_errors[0]} > {tol}"
+        )
+    return rvecs[0], tvecs[0]
---
+++ tests/test_geometry.py
@@ -0,0 +1,119 @@
+import numpy as np
+import pytest
+
+from mjecv.calibration import (
+    CheckerboardTarget,
+    PinholeNoneIntrinsics,
+    PinholeRadTanIntrinsics,
+)
+from mjecv.geometry import solve_pnp
+
+
+def test_solve_pnp_simple():
+    f = 10
+    p = 100
+    intrinsic_matrix = np.array([[f, 0, p], [0, f, p], [0, 0, 1]])
+    # cam = PinholeRadTanIntrinsics((f, f, p, p), (0, 0, 0, 0), 200, 200)
+    points = np.array([[0, 0, 0], [1, 1, 0], [2, 2, 0], [2, 0, 0]])
+    world_points = np.copy(points)
+    world_points[:, 2] += 2
+    image_points = p + f * world_points[:, :2] / world_points[:, 2].reshape(-1, 1)
+    rvec, tvec = solve_pnp(points, image_points, intrinsic_matrix, tol=0.1)
+    assert np.allclose(rvec, (0, 0, 0))
+    assert np.allclose(tvec, (0, 0, 2))
+
+
+poses = (
+    ((0, 0, 0), (0, 0, 1)),
+    ((0.5, 0, 0), (0, 0, 1)),
+    ((0, 0.5, 0), (0, 0, 1)),
+    ((0, 0, 0.5), (0, 0, 1)),
+    ((0.5, 0.5, 0), (0, 0, 1)),
+    ((0.5, 0, 0.5), (0, 0, 1)),
+    ((0, 0.5, 0.5), (0, 0, 1)),
+    ((0.5, 0.5, 0.5), (0, 0, 1)),
+)
+
+
+@pytest.mark.parametrize(
+    "cam",
+    (
+        PinholeNoneIntrinsics((10, 10, 100, 100), shape=(200, 200)),
+        PinholeRadTanIntrinsics(
+            (10, 10, 100, 100), (1e-3, 1e-4, 1e-5, 1e-6), shape=(200, 200)
+        ),
+    ),
+)
+@pytest.mark.parametrize("expected_rvec,expected_tvec", poses)
+def test_solve_pnp_pinhole(cam, expected_rvec, expected_tvec):
+    np.random.seed(0)
+    points = np.c_[np.random.uniform(-2, 2, (8, 2)), np.ones((8, 1))]
+    image_points = cam.project_points(points, expected_rvec, expected_tvec)
+
+    rvec, tvec = solve_pnp(
+        points, image_points, cam.intrinsic_matrix, cam.distortion_coeffs, tol=0.1
+    )
+    assert np.allclose(rvec, expected_rvec, atol=1e-6)
+    assert np.allclose(tvec, expected_tvec, atol=1e-6)
+
+
+@pytest.mark.parametrize("expected_rvec,expected_tvec", poses)
+def test_solvepnp_checkerboard(expected_rvec, expected_tvec):
+    cam = PinholeNoneIntrinsics((10, 10, 100, 100), shape=(200, 200))
+    target = CheckerboardTarget((8, 5), 0.1)
+    image_points = cam.project_points(
+        target.object_points, expected_rvec, expected_tvec
+    )
+    rvec, tvec = solve_pnp(
+        target.object_points, image_points, cam.intrinsic_matrix, tol=0.1
+    )
+    assert np.allclose(rvec, expected_rvec, atol=1e-10)
+    assert np.allclose(tvec, expected_tvec, atol=1e-10)
+
+
+@pytest.mark.parametrize("expected_rvec,expected_tvec", poses)
+def test_solvepnp_checkerboard_cm3(expected_rvec, expected_tvec):
+    # Intrinsics from Chameleon3 in slider experiment
+    cam = PinholeRadTanIntrinsics(
+        [1727.2, 1730.4, 638.2, 482.2],
+        [-9.97e-02, 1.8e-01, -7e-04, -1e-04],
+        1280,
+        1024,
+    )
+    # Checkerboard configuration from slider experiment
+    # TODO: add check for points outside image, which is what happens when this
+    #       checkerboard is bigger
+    target = CheckerboardTarget((8, 5), 0.035)
+    image_points = cam.project_points(
+        target.object_points, expected_rvec, expected_tvec
+    )
+
+    rvec, tvec = solve_pnp(
+        target.object_points,
+        image_points,
+        cam.intrinsic_matrix,
+        cam.distortion_coeffs,
+        tol=0.1,
+    )
+    assert np.allclose(rvec, expected_rvec, atol=1e-9)
+    assert np.allclose(tvec, expected_tvec, atol=1e-9)
+
+
+@pytest.mark.parametrize(
+    "bad_point",
+    ((-1, 100), (100, -1), (200, 100), (100, 210), (np.nan, np.nan), (np.inf, np.inf)),
+)
+def test_solvepnp_bad_points(bad_point):
+    cam = PinholeNoneIntrinsics((10, 10, 100, 100), shape=(200, 210))
+    target = CheckerboardTarget((8, 5), 0.1)
+    # TODO: similar checks in project points
+    image_points = cam.project_points(target.object_points, (0, 0, 0), (0, 0, 0))
+    image_points[0] = bad_point
+    with pytest.raises(ValueError):
+        solve_pnp(
+            target.object_points,
+            image_points,
+            cam.intrinsic_matrix,
+            cam.distortion_coeffs,
+            shape=cam.shape,
+        )
